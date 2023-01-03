9f6288a666e5264c26893825ad2c7cba8bef34a8
MuNeNICK <mune0127@munenick.me>
2023-01-03 13:24:40 +0000

    change docker-compose.yml ignore validate-class error

--- backend/app/src/main.ts
+++ backend/app/src/main.ts
@@ -4,7 +4,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    forbidUnknownValues: false
+  }));
   await app.listen(3000);
 }
 bootstrap();
